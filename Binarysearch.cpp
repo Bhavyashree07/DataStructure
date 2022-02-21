39a1c5f4a5cec7be5a20059c647252e2ae2d3e0e
Bhavyashree07 <97940064+Bhavyashree07@users.noreply.github.com>
2022-02-21 12:15:12 +0530

    Update Binarysearch.cpp

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -6,25 +6,25 @@ int main()
     cout<<"Enter 10 Elements (in ascending order) ";
     for(i=0; i<10; i++)
         cin>>arr[i];
-    cout<<"\nEnter Element to be Search: ";
+    cout<<"\nEnter Element to be Searched: ";
     cin>>num;
     first = 0;
     last = 9;
     middle = (first+last)/2;
-    while(first >= last)
+    while(first <= last)
     {
-        if(arr[middle]>num)
+        if(arr[middle]<num)
             first = middle+1;
         else if(arr[middle]==num)
         {
-            cout<<"\nThe number is found at Position ";
+            cout<<"\nThe number is found at Position "<<middle+1;
             break;
         }
         else
             last = middle-1;
         middle = (first+last)/2;
     }
-    if(first<last)
+    if(first>last)
         cout<<"\nThe number is not found in given Array"<<endl;
     return 0;
 }
