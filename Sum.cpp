57c228b3be3a627687f528158d4391c066c9f912
Bhavyashree07 <97940064+Bhavyashree07@users.noreply.github.com>
2022-02-21 12:44:41 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+
+	cout<<"Enter a and b values\n";
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"\n sum is="<<sum;
+	getch();
+}
